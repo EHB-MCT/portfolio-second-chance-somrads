c54677ccbfd1cb9daadf8f0ad8a1b89658d10387
somrad <somrad.sharma@student.ehb.be>
2023-08-10 15:03:03 +0200

    feat: Add testDB for integration testing

--- images/api/src/db/knexfile.js
+++ images/api/src/db/knexfile.js
@@ -1,7 +1,19 @@
 require("dotenv").config();
 
+const environment = process.env.NODE_ENV || "development";
+
+let connection;
+
+if (environment === "test") {
+  connection = process.env.POSTGRES_CONNECTION_STRING.replace(
+    `/${process.env.POSTGRES_DATABASE}`,
+    `/${process.env.POSTGRES_TEST_DATABASE}`
+  );
+} else {
+  connection = process.env.POSTGRES_CONNECTION_STRING;
+}
+
 module.exports = {
   client: "pg",
-  connection: process.env.POSTGRES_CONNECTION_STRING,
+  connection: connection,
 };
- 
