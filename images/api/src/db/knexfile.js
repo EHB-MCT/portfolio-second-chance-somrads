370d100f7ab6dc758d27a242143fa02945d51fb8
somrad <somrad.sharma@student.ehb.be>
2023-08-11 11:29:16 +0200

    feat: Recreated test db for testing purposes

--- images/api/src/db/knexfile.js
+++ images/api/src/db/knexfile.js
@@ -1,21 +1,6 @@
 require("dotenv").config();
 
-const environment = process.env.NODE_ENV || "development";
-
-let connection;
-
-if (environment === "test") {
-  connection = process.env.POSTGRES_CONNECTION_STRING.replace(
-    `/${process.env.POSTGRES_DATABASE}`,
-    `/${process.env.POSTGRES_TEST_DATABASE}`
-  );
-} else {
-  throw new Error("POSTGRES_CONNECTION_STRING is not defined");
-}
-
-console.log(`Using ${environment} database connection: ${connection}`);
-
 module.exports = {
   client: "pg",
-  connection: connection,
+  connection: process.env.POSTGRES_CONNECTION_STRING,
 };
