b6b6b753e68d10a20a281d8196d3d04e46710802
somrad <somrad.sharma@student.ehb.be>
2023-08-17 12:05:20 +0200

    feat: Add index to explain routes

--- images/api/src/app.js
+++ images/api/src/app.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 
 // Import necessary modules
 const express = require("express");
+const path = require("path"); // Added to handle file paths
 const app = express(); // Create an instance of an Express app
 const db = require("./db/db"); // Database connection and utility methods
 const usersRouter = require("./routes/users"); // Router for user-related endpoints
 const journalRouter = require("./routes/journal"); // Router for user-related endpoints
 const bodyParser = require("body-parser"); // Middleware to parse request bodies
-const cors = require('cors');
-
+const cors = require("cors");
 
 /**
  * Middleware to parse JSON bodies from HTTP requests
@@ -19,8 +19,12 @@ app.use(bodyParser.json());
 /**
  * Middleware to add CORS header
  */
+app.use(cors());
 
-app.use(cors()); 
+/**
+ * Middleware to serve static files from the public directory
+ */
+app.use(express.static(path.join(__dirname, "../public")));
 
 /**
  * Initialize and connect to the database
@@ -29,7 +33,7 @@ db.connect();
 
 /**
  * Healthcheck endpoint to ensure the service is running
- * 
+ *
  * @name Healthcheck
  * @route {GET} /healthcheck
  */
@@ -38,13 +42,13 @@ app.get("/healthcheck", (req, res) => {
 });
 
 /**
- * Root endpoint with a welcome message
- * 
+ * Root endpoint to serve the index.html file
+ *
  * @name Welcome
  * @route {GET} /
  */
 app.get("/", (req, res) => {
-  res.send({ message: "Hello, world!" });
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
 /**
@@ -59,7 +63,7 @@ app.use("/journal", journalRouter);
 
 /**
  * Export the app instance for use in other modules
- * 
+ *
  * @type {express.Application}
  */
 module.exports = app;
